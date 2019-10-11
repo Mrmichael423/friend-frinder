ecf65d528a1899bda55e231b39cd3c1e3c968750
Kevin Halstead <mrmichael423@icloud.com>
2019-10-11 09:58:22 -0600

    updated friends js with data

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+var matches = [
+  {
+    name: "Wolverine",
+    photo:
+      "https://static3.srcdn.com/wordpress/wp-content/uploads/2019/09/Wolverine-with-Metal-Claws-Comic.jpg",
+    choices: [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
+  },
+  {
+    name: "Captain America",
+    photo:
+      "https://www.goombastomp.com/wp-content/uploads/2019/05/CAptain-America.jpg",
+    choices: [1, 2, 3, 2, 5, 5, 2, 3, 1, 3]
+  },
+  {
+    name: "Iron man",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBAv17ZbzBiNOkLLTe8bQff4aZdAGSfpdGxG9ZJROmJBHdY8nS",
+    choices: [2, 5, 1, 4, 1, 3, 4, 1, 2, 1]
+  },
+  {
+    name: "Spider-man",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr8qsPoiyhXJ8pxyIdfpogoTjfcXgPaszrB7eW7dR2FQ95ZGLHqw",
+    choices: [3, 3, 1, 2, 4, 4, 2, 1, 5, 4]
+  },
+  {
+    name: "Hawkeye",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxAQWpWU53YMV2JfXb-MLLoxHjJnabWXMn0p9EeegyAylL4Eap",
+    choices: [1, 1, 3, 2, 1, 1, 5, 2, 1, 3]
+  },
+  {
+    name: "The Hulk",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzUQuJUIwYFA1Wq3sXxM6CdhKcpHrdTTSIbHk9YHhzrpZI2bLr",
+    choices: [4, 4, 4, 5, 5, 3, 4, 5, 4, 5]
+  },
+  {
+    name: "Thor",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEvH19uKIfhnkuQe4O5TqUrttmI-r1_R6ruawMVA3lrBoiUm5Y",
+    choices: [5, 5, 5, 4, 4, 5, 3, 5, 4, 1]
+  }
+];
