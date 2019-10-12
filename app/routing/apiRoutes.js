c2448ddcea0b1bfdc037ab2ef65172bdd91b1b19
Kevin Halstead <mrmichael423@icloud.com>
2019-10-12 17:32:05 -0600

    added a post and now can get user info on the back end. Converited the user choices from a string to a number

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,14 @@ module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
+
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body);
+    var input = req.body.choices;
+
+    for (var i = 0; i < input.length; i++) {
+      input[i] = parseInt(input[i]);
+      console.log(input[i]);
+    }
+  });
 };
